1d27c71e316e0acbc2939364064b1c83caf9c9d1
Wendy Alvarado <walvarado_r@hotmail.com>
2023-05-22 11:54:16 -0500

    mensaje de confirmacion

---
+++ EjApReC_01.java
@@ -0,0 +1,28 @@
+/*Realizar un programa para que una Persona pueda adoptar un Perro. Vamos a contar de dos
+clases. Perro, que tendrá como atributos: nombre, raza, edad y tamaño; y la clase Persona con
+atributos: nombre, apellido, edad, documento y Perro.
+Ahora deberemos en el main crear dos Personas y dos Perros. Después, vamos a tener que
+pensar la lógica necesaria para asignarle a cada Persona un Perro y por ultimo, mostrar desde
+la clase Persona, la información del Perro y de la Persona.
+ * 
+ */
+package ejaprec_01;
+
+import Entidades.Perro;
+import Entidades.Persona;
+
+/**
+ *
+ * @author Wendy
+ */
+public class EjApReC_01 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		Persona adoptar = new Persona();
+		adoptar.mostrarInformacion();
+
+	}
+}
